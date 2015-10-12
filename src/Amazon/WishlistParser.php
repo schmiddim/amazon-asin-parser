69a507a4b3f450308416975394dc33dd5983f394
ms <schmiddim@gmx.at>
2015-10-12 22:53:22 +0200

    wishlist parser

--- src/Amazon/WishlistParser.php
+++ src/Amazon/WishlistParser.php
@@ -10,5 +10,34 @@ namespace Amazon;
 
 class WishlistParser extends Parser
 {
+    /**
+     * @var null
+     */
+    private $wishlistId = null;
+
+    public function __construct($url)
+    {
+        parent::__construct($url);
+        $urlParameter = parse_url($this->getUrl());
+
+        $this->processWishListUrl($urlParameter);
+    }
+
+    protected function processWishListUrl($urlParameter)
+    {
+        $paramArray = explode('/', $urlParameter['path']);
+
+        $this->wishlistId = $paramArray[4];
+
+    }
+
+    /**
+     * @return null
+     */
+    public function getWishlistId()
+    {
+        return $this->wishlistId;
+    }
+
 
 }
